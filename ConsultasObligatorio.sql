12c11f6564c555c3f7573ef157560f373086e4e7
Zven88 <jota171088@gmail.com>
2021-08-26 02:23:02 -0300

    Consultas casi terminadas, revisar INSERT y  falta revisar dudas, falta agregar lo de indice que se va a dar en clase

---
+++ ConsultasObligatorio.sql
@@ -0,0 +1,152 @@
+--a.	Mostrar los datos del/los trene/s que pasaron por última vez por una línea-estación
+SELECT
+	P.codigoEstacion,
+	T.*
+FROM
+	TRENES T,
+	PASAN P
+WHERE
+	T.numero = P.numeroTren AND
+	P.fechaYHora = (SELECT TOP 1
+						P2.fechaYHora
+					FROM
+						PASAN P2
+					WHERE
+						P2.codigoEstacion = P.codigoEstacion
+					ORDER BY
+						P2.fechaYHora DESC)
+ORDER BY
+	P.codigoEstacion
+
+
+--b.	Mostrar los datos de las estaciones por las que pasaron más trenes este 
+--año que la cantidad promedio de trenes que pasaron en el año anterior.
+SELECT
+	E.codigo as codigoestacion,
+	COUNT(P.numeroTren) as cantidadTrenes
+FROM
+	ESTACIONES E,
+	PASAN P
+WHERE
+	E.codigo = P.codigoEstacion AND
+	YEAR(P.fechaYHora) = YEAR(GETDATE())
+GROUP BY
+	E.codigo
+HAVING
+	COUNT(P.numeroTren) > (SELECT
+								E2.codigo,
+								COUNT(P2.numeroTren)
+							FROM
+								PASAN P2,
+								ESTACIONES E2
+							WHERE
+								E2.codigo = p2.codigoEstacion AND
+								YEAR(P2.fechaYHora) = '2021'
+							GROUP BY
+								E2.codigo
+							
+									)
+
+
+
+--c.	Mostrar numero de línea, descripción, nombre de la estación inicio
+--nombre de la estación destino y cantidad de estaciones que la componen.
+SELECT
+	L.numero AS NumeroLinea,
+	L.descripcion AS Descripcion,
+	L.codigoEstacionOrigen,
+	L.codigoEstacionDestino,
+	COUNT(P.codigoEstacion) AS CantidadDeEstaciones
+FROM
+	LINEAS L,
+	POSEEN P,
+	ESTACIONES E
+WHERE
+	L.numero = P.numeroLinea AND
+	E.codigo = P.codigoEstacion
+GROUP BY
+	L.numero,
+	L.descripcion,
+	L.codigoEstacionOrigen,
+	L.codigoEstacionDestino
+
+
+	
+	
+
+--d.	Mostrar los datos de la línea que recorre la distancia más larga
+SELECT
+	L.*
+FROM
+	LINEAS L
+WHERE
+	L.longitud = (SELECT
+						MAX(L2.LONGITUD)
+					FROM
+						LINEAS L2)
+
+
+--e.	Mostrar los datos de las estaciones que están incluidas en líneas de color rojo 
+--pero no están incluidas en líneas de color amarillo.
+SELECT
+	DISTINCT E.*
+FROM
+	ESTACIONES E,
+	LINEAS L,
+	POSEEN P
+WHERE
+	P.codigoEstacion = E.codigo AND
+	P.numeroLinea = L.numero AND
+	L.color = 'rojo' AND 
+	P.codigoEstacion NOT IN (SELECT
+								P2.codigoEstacion
+							 FROM
+								POSEEN P2,
+								LINEAS L2
+							  WHERE
+								P2.numeroLinea = L2.numero AND
+								L2.color = 'amarillo')
+
+
+--f.	Mostrar los datos de los trenes que pasaron por todas las estaciones existentes.
+SELECT
+	T.*
+FROM
+	TRENES T
+WHERE
+	T.numero IN (SELECT
+					P.numeroTren
+				 FROM
+					PASAN P
+				 GROUP BY
+					P.numeroTren
+				 HAVING
+					COUNT(DISTINCT P.codigoEstacion) = (SELECT
+													       COUNT (E.codigo)
+													    FROM
+													       ESTACIONES E))
+													     
+
+--FORMA DESPROLIJA (muchos group by):
+
+--SELECT
+--	T.*
+--FROM
+--	TRENES T,
+--	PASAN P
+--WHERE
+--	T.numero = P.numeroTren
+--GROUP BY
+--	T.numero,
+--	T.descripcion,
+--	T.letra,
+--	T.capacVagon,
+--	T.cantVagon
+--HAVING
+--	COUNT(DISTINCT P.codigoEstacion) = (SELECT
+--											COUNT (E.codigo)
+--										FROM
+--											ESTACIONES E)
+
+
+
